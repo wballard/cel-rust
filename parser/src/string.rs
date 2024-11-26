4f2c62082e759fb6f060abc7b79fb4ceb4fec1e5
Will Ballard <wballard@mailframe.net>
2024-11-26 17:29:26 -0600

    test: double, raw, single escape strings

--- parser/src/string.rs
+++ parser/src/string.rs
@@ -1,27 +1,51 @@
-//! This example shows an example of how to parse an escaped string. The
-//! rules for the string are similar to JSON and rust. A string is:
-//!
-//! - Enclosed by double quotes
-//! - Can contain any raw unescaped code point besides \ and "
-//! - Matches the following escape sequences: \b, \f, \n, \r, \t, \", \\, \/
-//! - Matches code points like Rust: \u{XXXX}, where XXXX can be up to 6
-//!   hex characters
-//! - an escape followed by whitespace consumes all whitespace between the
-//!   escape and the next non-whitespace character
-
-use nom::branch::alt;
-use nom::bytes::complete::{tag, take_while_m_n};
-use nom::character::complete::char;
-//use nom::character::streaming::{char, hex_digit1, multispace1};
-use nom::combinator::{map, map_opt, map_res, value, verify};
-use nom::error::{FromExternalError, ParseError};
-use nom::multi::fold;
-use nom::sequence::{delimited, preceded};
-use nom::{AsChar, IResult, Parser};
+use chumsky::prelude::*;
 
 // parser combinators are constructed from the bottom up:
 // first we write parsers for the smallest elements (escaped characters),
 // then combine them into larger parsers.
+
+/// Parse the provided quoted string.
+/// This function was adopted from [snailquote](https://docs.rs/snailquote/latest/snailquote/).
+///
+/// # Details
+///
+/// Parses a single or double quoted string and interprets escape sequences such as
+/// '\n', '\r', '\'', etc.
+///
+/// " quoted strings allow escaping \"
+/// ' quoted strings allow escaping \'
+///
+/// Supports raw strings prefixed with `r` or `R` in which case all escape sequences are ignored.///
+///
+/// The full set of supported escapes between quotes may be found below:
+///
+/// | Escape     | Code       | Description                              |
+/// |------------|------------|------------------------------------------|
+/// | \a         | 0x07       | Bell                                     |
+/// | \b         | 0x08       | Backspace                                |
+/// | \v         | 0x0B       | Vertical tab                             |
+/// | \f         | 0x0C       | Form feed                                |
+/// | \n         | 0x0A       | Newline                                  |
+/// | \r         | 0x0D       | Carriage return                          |
+/// | \t         | 0x09       | Tab                                      |
+/// | \\         | 0x5C       | Backslash                                |
+/// | \?         | 0x??       | Question mark                            |
+/// | \"         | 0x22       | Double quote                             |
+/// | \'         | 0x27       | Single quote                             |
+/// | \`         | 0x60       | Backtick                                 |
+/// | \xDD       | 0xDD       | Unicode character with hex code DD       |
+/// | \uDDDD     | 0xDDDD     | Unicode character with hex code DDDD     |
+/// | \UDDDDDDDD | 0xDDDDDDDD | Unicode character with hex code DDDDDDDD |
+/// | \DDD       | 0DDD       | Unicode character with octal code DDD    |
+///
+pub fn parse_string<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> {
+    choice((
+        parse_single_quoted(),
+        parse_double_quoted(),
+        parse_single_quoted_raw(),
+        parse_double_quoted_raw(),
+    ))
+}
 
 /// Parse a unicode sequence, in forms:
 ///
@@ -30,59 +54,184 @@ use nom::{AsChar, IResult, Parser};
 /// * \uXXXX, where XXXX is 4 hexadecimal numerals
 /// * \UXXXXXXXX, where XXXXXXXX is 8 hexadecimal numerals
 /// * \DDD where DDD is 1 to 3 octal digits
-fn parse_unicode<'a, E>(input: &'a str) -> IResult<&'a str, char, E>
-where
-    E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
-{
-    let parse_delimited_hex = preceded(
-        char('u'),
-        delimited(
-            char('{'),
-            take_while_m_n(1, 6, |c: char| c.is_ascii_hexdigit()),
-            char('}'),
-        ),
+fn parse_unicode<'a>() -> impl Parser<'a, &'a str, char, extra::Err<Rich<'a, char>>> {
+    let escape_dual = just("\\x").ignore_then(text::digits(16).exactly(2).to_slice());
+    let escape_quad = just("\\u").ignore_then(text::digits(16).exactly(4).to_slice());
+    let escape_octet = just("\\U").ignore_then(text::digits(16).exactly(8).to_slice());
+    let bracketed =
+        (text::digits(16).at_least(1).at_most(6).to_slice()).delimited_by(just("u{"), just("}"));
+    let hex = choice((escape_dual, escape_quad, escape_octet, bracketed)).validate(
+        |digits, e, emitter| {
+            char::from_u32(u32::from_str_radix(digits, 16).unwrap()).unwrap_or_else(|| {
+                emitter.emit(Rich::custom(e.span(), "invalid unicode character"));
+                '\u{FFFD}' // unicode replacement character
+            })
+        },
     );
+    let octal = just("\\")
+        .ignore_then(text::digits(8).exactly(3).to_slice())
+        .validate(|digits, e, emitter| {
+            char::from_u32(u32::from_str_radix(digits, 8).unwrap()).unwrap_or_else(|| {
+                emitter.emit(Rich::custom(e.span(), "invalid unicode character"));
+                '\u{FFFD}' // unicode replacement character
+            })
+        });
+    choice((hex, octal))
+}
 
-    let parse_prefixed_hex_pair = preceded(
-        tag("\\x"),
-        take_while_m_n(1, 2, |c: char| c.is_ascii_hexdigit()),
-    );
-    let parse_prefixed_u_quad = preceded(
-        tag("\\u"),
-        take_while_m_n(4, 4, |c: char| c.is_ascii_hexdigit()),
-    );
-    let parse_prefixed_u_sextet = preceded(
-        tag("\\U"),
-        take_while_m_n(8, 8, |c: char| c.is_ascii_hexdigit()),
-    );
-    let parse_octal_triplet = preceded(tag("\\"), take_while_m_n(3, 3, |c: char| c.is_oct_digit()));
+/// The classic C style \ escape characters
+fn parse_escapes<'a>() -> impl Parser<'a, &'a str, char, extra::Err<Rich<'a, char>>> {
+    just('\\').ignore_then(choice((
+        just('\\').to('\\'),
+        just('/').to('/'),
+        just('"').to('"'),
+        just('\'').to('\''),
+        just('`').to('`'),
+        just('?').to('?'),
+        just('a').to('\x07'),
+        just('b').to('\x08'),
+        just('v').to('\x0B'),
+        just('f').to('\x0C'),
+        just('n').to('\n'),
+        just('r').to('\r'),
+        just('t').to('\t'),
+    )))
+}
 
-    let hex_formats = alt((
-        parse_prefixed_hex_pair,
-        parse_prefixed_u_quad,
-        parse_prefixed_u_sextet,
-        parse_delimited_hex,
-    ));
+fn parse_double_quoted<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> {
+    choice((none_of("\"\\"), parse_escapes(), parse_unicode()))
+        .repeated()
+        .collect::<String>()
+        .delimited_by(just('"'), just('"'))
+}
 
-    let parse_u32 = alt((
-        map_res(hex_formats, move |hex| u32::from_str_radix(hex, 16)),
-        map_res(parse_octal_triplet, move |oct| u32::from_str_radix(oct, 8)),
-    ));
+fn parse_single_quoted<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> {
+    choice((none_of("'\\"), parse_escapes(), parse_unicode()))
+        .repeated()
+        .collect::<String>()
+        .delimited_by(just("'"), just("'"))
+}
 
-    map_opt(parse_u32, std::char::from_u32).parse(input)
+fn parse_double_quoted_raw<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> {
+    choice((just("r"), just("R"))).ignore_then(
+        choice((none_of("\"\\"), just("\\\"").to('"'), just("\\").to('\\')))
+            .repeated()
+            .collect::<String>()
+            .delimited_by(just('"'), just('"')),
+    )
+}
+
+fn parse_single_quoted_raw<'a>() -> impl Parser<'a, &'a str, String, extra::Err<Rich<'a, char>>> {
+    choice((just("r"), just("R"))).ignore_then(
+        choice((none_of("'\\"), just("\\'").to('\''), just("\\").to('\\')))
+            .repeated()
+            .collect::<String>()
+            .delimited_by(just('\''), just('\'')),
+    )
 }
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
+    use rstest::rstest;
 
     #[test]
     fn test_parse_unicode() {
-        assert_eq!(parse_unicode::<()>("u{1F600}"), Ok(("", '😀')));
-        assert_eq!(parse_unicode::<()>("\\u270C"), Ok(("", '✌')));
-        assert_eq!(parse_unicode::<()>("\\xAC"), Ok(("", '¬')));
-        assert_eq!(parse_unicode::<()>("\\U0001f431"), Ok(("", '🐱')));
-        assert_eq!(parse_unicode::<()>("\\254"), Ok(("", '¬')));
+        assert_eq!(parse_unicode().parse("\\u270C").unwrap(), '✌');
+        assert_eq!(parse_unicode().parse("\\xAC").unwrap(), '¬');
+        assert_eq!(parse_unicode().parse("\\U0001f431").unwrap(), '🐱');
+        assert_eq!(parse_unicode().parse("u{1F600}").unwrap(), '😀');
+        assert_eq!(parse_unicode().parse("\\254").unwrap(), '¬');
+    }
+
+    #[test]
+    fn test_parse_escape() {
+        assert_eq!(parse_escapes().parse("\\a").unwrap(), '\u{07}');
+    }
+    #[test]
+    fn test_parse_unicode_fail() {
+        assert_eq!(
+            parse_unicode()
+                .parse("\\u27ZC")
+                .into_errors()
+                .first()
+                .unwrap()
+                .to_string(),
+            "found Z expected something else"
+        );
+    }
+
+    #[rstest]
+    #[case("\"Hello\"", String::from("Hello"))]
+    #[case("\"Hello \\a\"", String::from("Hello \u{07}"))]
+    #[case("\"Hello \\b\"", String::from("Hello \u{08}"))]
+    #[case("\"Hello \\v\"", String::from("Hello \u{0b}"))]
+    #[case("\"Hello \\f\"", String::from("Hello \u{0c}"))]
+    #[case("\"Hello \\n\"", String::from("Hello \u{0a}"))]
+    #[case("\"Hello \\r\"", String::from("Hello \u{0d}"))]
+    #[case("\"Hello \\t\"", String::from("Hello \u{09}"))]
+    #[case("\"Hello \\\\\"", String::from("Hello \\"))]
+    #[case("\"Hello \\?\"", String::from("Hello ?"))]
+    #[case("\"Hello \\\"\"", String::from("Hello \""))]
+    #[case("\"Hello \\'\"", String::from("Hello '"))]
+    #[case("\"Hello \\`\"", String::from("Hello `"))]
+    #[case("\"Hello \\x20 \"", String::from("Hello   "))]
+    #[case("\"Hello \\x60\"", String::from("Hello `"))]
+    #[case("\"Hello \\u270c\"", String::from("Hello ✌"))]
+    #[case("\"Hello \\U0001f431\"", String::from("Hello 🐱"))]
+    fn double_quotes_interprets_escapes(#[case] script: &str, #[case] expected: String) {
+        let result = parse_string().parse(script);
+        assert_eq!(result.unwrap(), expected)
+    }
+
+    #[rstest]
+    #[case("'Hello'", String::from("Hello"))]
+    #[case("'Hello \\a'", String::from("Hello \u{07}"))]
+    #[case("'Hello \\b'", String::from("Hello \u{08}"))]
+    #[case("'Hello \\v'", String::from("Hello \u{0b}"))]
+    #[case("'Hello \\f'", String::from("Hello \u{0c}"))]
+    #[case("'Hello \\n'", String::from("Hello \u{0a}"))]
+    #[case("'Hello \\r'", String::from("Hello \u{0d}"))]
+    #[case("'Hello \\t'", String::from("Hello \u{09}"))]
+    #[case("'Hello \\\\'", String::from("Hello \\"))]
+    #[case("'Hello \\?'", String::from("Hello ?"))]
+    #[case("'Hello \\\"'", String::from("Hello \""))]
+    #[case("'Hello \\''", String::from("Hello '"))]
+    #[case("'Hello \\`'", String::from("Hello `"))]
+    #[case("'Hello \\x20 '", String::from("Hello   "))]
+    #[case("'Hello \\x60'", String::from("Hello `"))]
+    #[case("'Hello \\u270c'", String::from("Hello ✌"))]
+    #[case("'Hello \\U0001f431'", String::from("Hello 🐱"))]
+    fn single_quotes_interprets_escapes(#[case] script: &str, #[case] expected: String) {
+        let result = parse_string().parse(script);
+        assert_eq!(result.unwrap(), expected)
+    }
+
+    #[rstest]
+    #[case(
+        "r\"Hello \\a \\\" ' \\' \\U0001f431 \"",
+        String::from("Hello \\a \" ' \\' \\U0001f431 ")
+    )]
+    #[case(
+        "R\"Hello \\a \\\" ' \\' \\U0001f431 \"",
+        String::from("Hello \\a \" ' \\' \\U0001f431 ")
+    )]
+    fn raw_double_quotes(#[case] script: &str, #[case] expected: String) {
+        let result = parse_string().parse(script);
+        assert_eq!(result.unwrap(), expected)
+    }
+    #[rstest]
+    #[case(
+        "r'Hello \\a \\\" \" \\' \\U0001f431 '",
+        String::from("Hello \\a \\\" \" ' \\U0001f431 ")
+    )]
+    #[case(
+        "R'Hello \\a \\\" \" \\' \\U0001f431 '",
+        String::from("Hello \\a \\\" \" ' \\U0001f431 ")
+    )]
+    fn raw_single_quotes(#[case] script: &str, #[case] expected: String) {
+        let result = parse_string().parse(script);
+        assert_eq!(result.unwrap(), expected)
     }
 }
